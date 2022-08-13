b7b4bd91d474ec7a6113fa7ddd8891f97b79b659
CalebStock <calebstock91@gmail.com>
2022-08-13 16:23:25 -0500

    added function to turn seconds into a string of days/hours/minutes

---
+++ general.py
@@ -0,0 +1,16 @@
+def Convert_time(n):
+    day = int(n // (24 * 3600))
+    n = n % (24 * 3600)
+    hour = int(n // 3600)
+    n %= 3600
+    minutes = int(n // 60)
+    m_plural="s" if minutes>1 else""
+    m=f"{minutes} Minute{m_plural}" if minutes else ""
+    delimiter_2="   ||   " if minutes and hour or minutes and day else ""
+    h_plural="s" if hour>1 else""
+    h=f"{hour} Hour{h_plural}" if hour else ""
+    delimiter_1="   ||   " if hour and day else ""
+    d_plural="s" if day>1 else""
+    d=f"{day} Day{d_plural}" if day else ""
+    product=f"{d}{delimiter_1}{h}{delimiter_2}{m}" if day or hour or minutes else "Run time less than a minute"
+    return product
